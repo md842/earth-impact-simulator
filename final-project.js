787ffb32347e2cfb1fc366769d7b71ee12ae4730
Michelle Kim <mkheeseo@gmail.com>
2023-12-06 18:55:56 -0800

    Updated stars, used Max's scale factor

--- final-project.js
+++ final-project.js
@@ -16,10 +16,12 @@ export class FinalProject extends Scene {
             s5: new defs.Subdivision_Sphere(5),
         };
 
+        this.scale = 3.0; // For Michelle
+
         this.launch = this.hit = this.reset = this.destroy = this.cratered = false;
         this.projectile_speed = 100000;
-        this.projectile_pos = 300;
-        this.projectile_size = 250000;
+        this.projectile_pos = 10 * this.scale;
+        this.projectile_size = 500000;
 
         // Define the materials used to draw the Earth and its moon.
         const bump = new defs.Fake_Bump_Map(1);
@@ -31,15 +33,20 @@ export class FinalProject extends Scene {
                 ambient: 1, diffusivity: 0.1, specularity: 0.1,
                 texture: new Texture("assets/earth.gif", "LINEAR_MIPMAP_LINEAR")
             }),
-            destroyed_earth: new Material(new defs.Phong_Shader(), 
+            destroyed_earth: new Material(new defs.Phong_Shader(),
                 {specularity: 0.75, diffusivity: 1, ambient: 0.25, color: hex_color("#FF0000")}),
             moon: new Material(bump,
                 {specularity: 0.75, diffusivity: 1, ambient: 1, texture: new Texture("assets/moon.jpg")}),
-            projectile: new Material(new defs.Phong_Shader(), 
+            projectile: new Material(new defs.Phong_Shader(),
                 {specularity: 0.75, diffusivity: 1, ambient: 1, color: hex_color("#FF0000")}),
+            stars: new Material(new defs.Phong_Shader(),
+                {specularity: 0.75, diffusivity: 1, ambient: 0.25, color: hex_color("#FFFFFF")}),
         }
 
-        this.initial_camera_location = Mat4.look_at(vec3(0, 100, 450), vec3(0, 0, 0), vec3(0, 1, 0));
+        this.initial_camera_location = Mat4.look_at(vec3(0, 3 * this.scale, 15 * this.scale), vec3(0, 0, 0), vec3(0, 1, 0));
+
+        this.star_transforms = []
+        this.star_colors = [hex_color("#ffa6a6"),hex_color("#ffcaa6"),hex_color("#ffeca6"),hex_color("#a6fff9"),hex_color("#a6d4ff"),hex_color("#a6b0ff")]
     }
 
     make_control_panel() {
@@ -48,12 +55,12 @@ export class FinalProject extends Scene {
         this.key_triggered_button("Dynamic Camera View", ["Control", "d"], () => this.attached = () => this.projectile);
         this.new_line();
         this.new_line();
-        
+
         this.make_slider("Projectile Velocity (m/s):", () => this.projectile_speed = this.slider_value, 10000, 299792457, 100000);
         this.live_string(box=>box.textContent = "Velocity in terms of c: " + (this.projectile_speed / 299792458).toFixed(12) + "c");
         this.new_line();
         // Lorentz factor = 1 / sqrt(1 - v^2/c^2) where c is speed of light in vacuum
-        this.live_string(box=>box.textContent = "Lorentz factor: γ = " + (1 / Math.sqrt(1 - (this.projectile_speed ** 2)/(299792458 ** 2))).toFixed(12));
+        this.live_string(box=>box.textContent = "Lorentz factor: Î³ = " + (1 / Math.sqrt(1 - (this.projectile_speed ** 2)/(299792458 ** 2))).toFixed(12));
 
         this.new_line();
         this.new_line();
@@ -83,18 +90,18 @@ export class FinalProject extends Scene {
         }
 
         // Define the program's lights
-        program_state.lights = [new Light(vec4(0, 0, 0, 1), color(1, 1, 1, 1), 1000)];
+        program_state.lights = [new Light(vec4(0, 0, 0, 1), color(1, 1, 1, 1), 10000)];
 
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, .1, 1000);
-        
+
         const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
 
         let model_transform = Mat4.identity();
 
         // EARTH
         const rotation_multiplier = 0.25; // Control the rotation speed of Earth on its axis
-        let earth_transform = model_transform.times(Mat4.rotation(t * rotation_multiplier, t, t / (rotation_multiplier ** 2), 1).times(Mat4.scale(15, 15, 15)));
+        let earth_transform = model_transform.times(Mat4.rotation(t * rotation_multiplier, t, t / (rotation_multiplier ** 2), 1).times(Mat4.scale(this.scale, this.scale, this.scale)));
         if(this.destroy) // Earth was destroyed by impact, draw with a different material
             this.shapes.s5.draw(context, program_state, earth_transform, this.materials.destroyed_earth);
         else if (this.cratered) // Earth was cratered by impact, draw with a different material
@@ -109,10 +116,10 @@ export class FinalProject extends Scene {
         this.moon = moon_transform;
 
         // PROJECTILE
-        let scale_factor = 15.0 * this.projectile_size / 6378100.0;
+        let scale_factor = this.scale * this.projectile_size / 6378100.0;
         let projectile_transform = model_transform.times(Mat4.translation(0, 0, this.projectile_pos)).times(Mat4.scale(scale_factor, scale_factor, scale_factor));
-   
-        if(this.projectile_pos + scale_factor < 15){ // Hit detection; scale factor of Earth is 15
+
+        if(this.projectile_pos + scale_factor < this.scale){ // Hit detection; scale factor of Earth is 15
             this.hit = true;
             // Relativistic kinetic energy: (Lorentz factor - 1)(m_0)(c^2) where m_0 is mass at rest and c is speed of light
             let relativistic_kinetic_energy = ((1 / Math.sqrt(1 - (this.projectile_speed ** 2)/(299792458 ** 2))) - 1) * this.projectile_size * 299792458 ** 2;
@@ -126,13 +133,47 @@ export class FinalProject extends Scene {
 
         if(this.reset){
             this.launch = this.hit = this.reset = this.destroy = this.cratered = false;
-            this.projectile_pos = 300;
+            this.projectile_pos = 10 * this.scale;
         }
 
         if (!this.hit)
             if(this.launch)
-                this.projectile_pos -= 15.0 * this.projectile_speed / 6378100.0; // Move projectile towards Earth based on scale factor
-            this.shapes.s5.draw(context, program_state, projectile_transform, this.materials.projectile); // Draw projectile based on position; will not be drawn after impact
+                this.projectile_pos -= this.scale * this.projectile_speed / 6378100.0; // Move projectile towards Earth based on scale factor
+        this.shapes.s5.draw(context, program_state, projectile_transform, this.materials.projectile); // Draw projectile based on position; will not be drawn after impact
+
+        // STARS
+        function rand_int(min, max) {return Math.round(100 * (min + Math.random() * max)) / 100;}
+        function rand_pos_neg() {return Math.random() < 0.5 ? -1 : 1;}
+
+        let star_transform = model_transform;
+        let star_count = 300;
+        let star_field_min_radius = 15;     let star_field_max_radius = 40;
+        let star_min_size = 0.05;           let star_max_size = 0.1;
+
+        // call star randomizer once
+        for (let i = 0; i < star_count; i++) {
+            star_transform = model_transform;
+            let star_scale = rand_int(star_min_size, star_max_size);
+            let star_trans_x = rand_pos_neg() * rand_int(0, star_field_max_radius);
+            let star_trans_y = rand_pos_neg() * rand_int(0, star_field_max_radius);
+            let star_trans_z = rand_pos_neg() * rand_int(0, star_field_max_radius);
+            // re-roll if point under minimum radius
+            while (Math.sqrt(star_trans_x ** 2 + star_trans_y ** 2 + star_trans_z ** 2) < star_field_min_radius) {
+                star_trans_x = rand_pos_neg() * rand_int(star_field_min_radius, star_field_max_radius);
+                star_trans_y = rand_pos_neg() * rand_int(star_field_min_radius, star_field_max_radius);
+                star_trans_z = rand_pos_neg() * rand_int(star_field_min_radius, star_field_max_radius);
+            }
+            star_transform = model_transform.times(Mat4.translation(star_trans_x, star_trans_y, star_trans_z)).times(Mat4.scale(star_scale,star_scale,star_scale));
+            if (this.star_transforms.length < star_count) {
+                this.star_transforms.push(star_transform);
+            }
+        }
+
+        // draw stars
+        for (let i = 0; i < star_count; i++) {
+            this.shapes.s5.draw(context, program_state, this.star_transforms[i], this.materials.stars.override({color:this.star_colors[i%6]}));
+        }
+
 
         // camera movement
         this.projectile = projectile_transform;
